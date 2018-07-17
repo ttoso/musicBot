5439d9ab56e212dd7ac1f84d9bf76af6a9859eb6
ttoso <toso50000@gmail.com>
2018-07-17 12:41:57 +0200

    Added code and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Spotipy Bot
+
+This is a little script for a music classifiyng bot using spotipy library, it gets an artist name and return all his genres registered on spotify database.
---
+++ test.py
@@ -0,0 +1,19 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+def getGenres(artistName):
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    genres = []
+
+    results = sp.search(artistName, type='artist')
+    generos = results['artists']['items'][0]['genres']
+    for genero in generos:
+        genres.append(genero)
+    return genres
+
+
+result = getGenres('rammstein')
+for genero in result:
+    print(genero)
